bff9042ac7e29eadab3f141e5f7e03eb6f1449b7
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-09-27 20:24:24 +0300

    Update StudentMarks.java

--- StudentMarks/src/StudentMarks.java
+++ StudentMarks/src/StudentMarks.java
@@ -1,26 +1,19 @@
 public class StudentsResult {
 
     static void StudentResult(String name, int mark) {
-        char grade;
-
         if (mark >= 0 && mark <= 49) {
-            grade = 'F';
+            System.out.println("Please correct your situation " + name + ", your score is F");
         } else if (mark >= 50 && mark <= 60) {
-            grade = 'C';
+            System.out.println("Not bad " + name + ", your score is C");
         } else if (mark >= 61 && mark <= 70) {
-            grade = 'D';
+            System.out.println("Good " + name + ", your score is D+");
         } else if (mark >= 71 && mark <= 80) {
-            grade = 'C';
+            System.out.println("Very Good " + name + ", your score is C");
         } else if (mark >= 81 && mark <= 90) {
-            grade = 'B';
+            System.out.println("Excellent " + name + ", your score is B");
         } else if (mark >= 91 && mark <= 100) {
-            grade = 'A';
-        } else {
-            System.out.println("Invalid mark for " + name);
-            return;
+            System.out.println("Outstanding " + name + ", your score is A");
         }
-
-        System.out.println(name + ", your score is " + grade);
     }
 
     public static void main(String[] args) {
